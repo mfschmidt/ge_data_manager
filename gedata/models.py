6c30cf47379a77650ea320ab861389c87534eaa5
mfschmidt <mikeschmidt@schmidtgracen.com>
2019-08-07 12:51:31 -0400

    Changed long database field names to match short BIDS names Added 1.0 BIDS fields Grew a few max_lengths on varchars to fit existing runs

--- gedata/models.py
+++ gedata/models.py
@@ -16,23 +16,26 @@ class PushResult(models.Model):
     start_date = models.DateTimeField('date_started')
     end_date = models.DateTimeField('date_completed')
     host = models.CharField(max_length = 16)
-    command = models.CharField(max_length = 256)
-    version = models.CharField(max_length = 16)
+    command = models.CharField(max_length = 384)
+    version = models.CharField(max_length = 64)  # 'running uninstalled without version' longest
     # duration is in seconds
     duration = models.IntegerField()
 
     # Data preparation and execution details
     shuffle = models.CharField(max_length = 16)  # 'derivatives': 'derivatives', 'agnostic': 'shuffles', 'distance': 'distshuffles', 'edges': 'edgeshuffles'
-    subject = models.CharField(max_length = 32)
-    hemisphere = models.CharField(max_length = 1)
-    cortex = models.CharField(max_length = 3)
-    probes = models.CharField(max_length = 16)
-    target = models.CharField(max_length = 3)
-    algorithm = models.CharField(max_length = 8)
-    normalization = models.CharField(max_length = 16)
-    comparator = models.CharField(max_length = 32)
+    sub = models.CharField(max_length = 32)
+    hem = models.CharField(max_length = 1)
+    samp = models.CharField(max_length = 16)
+    prob = models.CharField(max_length = 16)
+    parby = models.CharField(max_length = 16)
+    splby = models.CharField(max_length = 16)
+    batch = models.CharField(max_length = 16)
+    tgt = models.CharField(max_length = 3)
+    algo = models.CharField(max_length = 8)
+    norm = models.CharField(max_length = 16)
+    comp = models.CharField(max_length = 48)
     mask = models.CharField(max_length = 32)
-    adjustment = models.CharField(max_length = 16)
+    adj = models.CharField(max_length = 16)
     seed = models.IntegerField()
 
     # Result details
@@ -42,12 +45,12 @@ class PushResult(models.Model):
     # Representations
     def __str__(self):
         if self.shuffle == "derivatives":
-            return "{} -x- {} to {} via {}".format(
-                self.subject, self.comparator, self.target, self.algorithm
+            return "{}-{} -x- {} to {} via {}".format(
+                self.parby, self.splby, self.comp, self.tgt, self.algo
             )
         else:
-            return "{}: {} -x- {} to {} via {}, seed={}".format(
-                self.shuffle, self.subject, self.comparator, self.target, self.algorithm, self.seed,
+            return "{}: {}-{} -x- {} to {} via {}, seed={}".format(
+                self.shuffle, self.parby, self.splby, self.comp, self.tgt, self.algo, self.seed,
             )
 
 
