b84a8597039024973a8fd4649cde0accee5e8b85
mfschmidt <mikeschmidt@schmidtgracen.com>
2019-08-27 15:39:33 -0400

    up-sized success and error icons added message to spinners

--- celery_progress/static/celery_progress/celery_progress.js
+++ celery_progress/static/celery_progress/celery_progress.js
@@ -63,24 +63,33 @@ var CelerySpinner = (function () {
 
     function onSuccessDefault(spinnerElement, dataForLater) {
         spinnerElement.style.color = '#76ce60';
-        spinnerElement.innerHTML = '<i class="fa fa-smile"></i>';
+        html_for_smile = '<div style="text-align: center;"><span class="fa-4x">';
+        html_for_smile += '<i class="fa fa-smile"></i>';
+        html_for_smile += '</span></div>';
+        spinnerElement.innerHTML = html_for_smile;
     }
 
     function onErrorDefault(spinnerElement) {
         spinnerElement.style.color = '#dc4f63';
-        spinnerElement.innerHTML = '<i class="fa fa-poo-storm"></i>';
+        html_for_failure = '<div style="text-align: center;"><span class="fa-4x">';
+        html_for_failure += '<i class="fa fa-poo-storm"></i>';
+        html_for_failure += '</span></div>';
+        spinnerElement.innerHTML = html_for_failure;
     }
 
-    function onProgressDefault(spinnerInnerElement, progress) {
+    function onProgressDefault(spinnerInnerElement, spinnerMessageElement,  progress) {
         spinnerInnerElement.innerHTML = Math.round(progress.percent) + '%';
+        spinnerMessageElement.innerHTML = progress.message;
     }
 
     function updateProgress(progressUrl, options) {
         options = options || {};
         let spinnerId = options.spinnerId || 'spinner';
         let spinnerInnerId = spinnerId + "_inner";
+        let spinnerMessageId = spinnerId + "_message";
         let spinnerElement = options.spinnerElement || document.getElementById(spinnerId);
         let spinnerInnerElement = options.spinnerInnerElement || document.getElementById(spinnerInnerId);
+        let spinnerMessageElement = options.spinnerMessageElement || document.getElementById(spinnerMessageId);
         let onProgress = options.onProgress || onProgressDefault;
         let onSuccess = options.onSuccess || onSuccessDefault;
         let onError = options.onError || onErrorDefault;
@@ -94,16 +103,18 @@ var CelerySpinner = (function () {
             html_for_spinner = '<div style="text-align: center;"><span class="fa-stack fa-4x">';
             html_for_spinner += '<i class="fa fa-spinner fa-spin fa-stack-2x"></i>';
             html_for_spinner += '<strong id="' + spinnerInnerId + '" class="fa-stack-1x pct_text"></strong>';
-            html_for_spinner += '</span></div>';
+            html_for_spinner += '</span>';
+            html_for_spinner += '<p id="' + spinnerMessageId + '"></p></div>';
             spinnerElement.innerHTML = html_for_spinner;
             spinnerInnerElement = document.getElementById(spinnerInnerId);
+            spinnerMessageElement = document.getElementById(spinnerMessageId);
             options.alreadyStarted = "True";
         }
 
         fetch(progressUrl).then(function(response) {
             response.json().then(function(data) {
                 if (data.progress) {
-                    onProgress(spinnerInnerElement, data.progress);
+                    onProgress(spinnerInnerElement, spinnerMessageElement, data.progress);
                 }
                 if (!data.complete) {
                     setTimeout(updateProgress, pollInterval, progressUrl, options);
