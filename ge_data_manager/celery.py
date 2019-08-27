d0142d6329466229546d1413403360dc0c47988d
mfschmidt <mikeschmidt@schmidtgracen.com>
2019-08-26 23:33:26 -0400

    Added celery_plots_in_progress function to prevent duplicating plot building

--- ge_data_manager/celery.py
+++ ge_data_manager/celery.py
@@ -56,4 +56,42 @@ def celery_id_from_name(job_name):
                 if job_name in d['name']:
                     return d['id']
 
+    """ One example of a task_dict:
+    [{
+        'id': '97b8c300-9193-4834-9a04-0472997e5d54',
+        'name': 'gedata.tasks.build_plot',
+        'args': "('HCPGW64',)",
+        'kwargs': "{'data_path': '/data'}",
+        'type': 'gedata.tasks.build_plot',
+        'hostname': 'celery@mq',
+        'time_start': 1566873261.7720587,
+        'acknowledged': True,
+        'delivery_info': {
+            'exchange': '',
+            'routing_key': 'celery',
+            'priority': 0,
+            'redelivered': False
+        },
+        'worker_pid': 26
+    }]
+    """
     return None
+
+def celery_plots_in_progress():
+    try:
+        task_dict = app.control.inspect().active()
+    except ConnectionResetError:
+        task_dict = app.control.inspect().active()
+
+    plots = []
+
+    if task_dict is None:
+        return plots
+
+    if len(task_dict) > 0:
+        for k in task_dict.keys():
+            for d in task_dict[k]:
+                if 'build_plot' in d['name']:
+                    plots.append("train_test_{}.png".format(d['args'][2:9].lower()))
+
+    return plots
