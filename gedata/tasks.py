9463bf26d7e00e83ae90f56218c86ce15d81a237
mfschmidt <mikeschmidt@schmidtgracen.com>
2020-07-11 23:16:20 -0400

    consolidated one_mask into PyGEST.algorithms

--- gedata/tasks.py
+++ gedata/tasks.py
@@ -293,115 +293,6 @@ def comp_from_signature(signature, filename=False):
     return comp_map[signature.lower()]
 
 
-def one_mask(df, mask_type, sample_type, data_dir="/data"):
-    """ return a vector of booleans from the lower triangle of a matching-matrix based on 'mask_type'
-
-    :param df: pandas.DataFrame with samples as columns
-    :param str mask_type: A list of strings to specify matching masks, or a minimum distance to mask out
-    :param str sample_type: Samples can be 'wellid' or 'parcelid'
-    :param data_dir: The root of the path to all pygest data
-    :return: Boolean 1-D vector to remove items (False values in mask) from any sample x sample triangle vector
-    """
-
-    # If mask is a number, use it as a distance filter
-    data = ge.Data(data_dir, null_handler)
-    try:
-        # Too-short values to mask out are False, keepers are True.
-        min_dist = float(mask_type)
-        mask_vector = np.array(
-            data.distance_vector(df.columns, sample_type=sample_type) > min_dist,
-            dtype=bool
-        )
-        print("        masking out {:,} of {:,} edges closer than {}mm apart.".format(
-            np.count_nonzero(np.invert(mask_vector)), len(mask_vector), min_dist
-        ))
-        return mask_vector
-    except TypeError:
-        pass
-    except ValueError:
-        pass
-
-    # Mask is not a number, see if it's a pickled dataframe
-    if os.path.isfile(mask_type):
-        with open(mask_type, 'rb') as f:
-            mask_df = pickle.load(f)
-        if isinstance(mask_df, pd.DataFrame):
-            # Note what we started with so we can report after we tweak the dataframe.
-            # Too-variant values to mask out are False, keepers are True.
-            orig_vector = mask_df.values[np.tril_indices(n=mask_df.shape[0], k=-1)]
-            orig_falses = np.count_nonzero(~orig_vector)
-            orig_length = len(orig_vector)
-            print("  M  found {} containing {:,} x {:,} mask".format(mask_type, mask_df.shape[0], mask_df.shape[1]))
-            print("  M    generating {:,}-len vector with {:,} False values to mask.".format(orig_length, orig_falses))
-
-            # We can only use well_ids found in BOTH df and our new mask, make shapes match.
-            unmasked_ids = [well_id for well_id in df.columns if well_id not in mask_df.columns]
-            usable_ids = [well_id for well_id in df.columns if well_id in mask_df.columns]
-            usable_df = mask_df.reindex(index=usable_ids, columns=usable_ids)
-            usable_vector = usable_df.values[np.tril_indices(n=len(usable_ids), k=-1)]
-            usable_falses = np.count_nonzero(~usable_vector)
-            usable_length = len(usable_vector)
-            print("  M  {:,} well_ids not found in the mask; padding with Falses.".format(len(unmasked_ids)))
-
-            pad_rows = pd.DataFrame(np.zeros((len(unmasked_ids), len(mask_df.columns)), dtype=bool),
-                                    columns=mask_df.columns, index=unmasked_ids)
-            mask_df = pd.concat([mask_df, pad_rows], axis=0)
-            pad_cols = pd.DataFrame(np.zeros((len(mask_df.index), len(unmasked_ids)), dtype=bool),
-                                    columns=unmasked_ids, index=mask_df.index)
-            mask_df = pd.concat([mask_df, pad_cols], axis=1)
-            mask_vector = mask_df.values[np.tril_indices(n=mask_df.shape[0], k=-1)]
-            mask_falses = np.count_nonzero(~mask_vector)
-            mask_trues = np.count_nonzero(mask_vector)
-            print("  M  padded mask matrix out to {:,} x {:,}".format(
-                mask_df.shape[0], mask_df.shape[1]
-            ))
-            print("  M    with {:,} True, {:,} False, {:,} NaNs in triangle.".format(
-                mask_trues, mask_falses, np.count_nonzero(mask_vector.isnan())
-            ))
-
-            shaped_mask_df = mask_df.reindex(index=df.columns, columns=df.columns)
-            shaped_vector = shaped_mask_df.values[np.tril_indices(n=len(df.columns), k=-1)]
-            print("  M  masking out {:,} (orig {:,}, {:,} usable) hi-var".format(
-                np.count_nonzero(~shaped_vector), orig_falses, usable_falses,
-            ))
-            print("  M    of {:,} (orig {:,}, {:,} usable) edges.".format(
-                len(shaped_vector), orig_length, usable_length
-            ))
-            return shaped_vector
-        else:
-            print("  M  {} is a file, but not a pickled dataframe. Skipping this mask.".format(mask_type))
-            do_nothing_mask = np.ones((len(df.columns), len(df.columns)), dtype=bool)
-            return do_nothing_mask[np.tril_indices(n=len(df.columns), k=-1)]
-
-    # Mask is not a number, so treat it as a matching filter
-    if mask_type[:4] == 'none':
-        items = list(df.columns)
-    elif mask_type[:4] == 'fine':
-        items = data.samples(samples=df.columns)['fine_name']
-    elif mask_type[:6] == 'coarse':
-        items = data.samples(samples=df.columns)['coarse_name']
-    else:
-        items = data.samples(samples=df.columns)['structure_name']
-    mask_array = np.ndarray((len(items), len(items)), dtype=bool)
-
-    # There is, potentially, a nice vectorized way to mark matching values as True, but I can't find it.
-    # So, looping works and is easy to read, although it might cost us a few extra ms.
-    for i, y in enumerate(items):
-        for j, x in enumerate(items):
-            # Generate one edge of the match matrix
-            mask_array[i][j] = True if mask_type == 'none' else (x != y)
-    mask_vector = mask_array[np.tril_indices(n=mask_array.shape[0], k=-1)]
-
-    print("        masking out {:,} of {:,} '{}' edges.".format(
-        sum(np.invert(mask_vector)), len(mask_vector), mask_type
-    ))
-
-    # if len(mask_vector) == 0:
-    #     mask_vector = np.ones(int(len(df.columns) * (len(df.columns) - 1) / 2), dtype=bool)
-
-    return mask_vector
-
-
 def test_score(tsv_file, base_path='/data', own_expr=False, mask='none', probe_significance_threshold=None):
     """ Use tsv_file to figure out its complement test expression dataframe.
         Use the trained probe list to index the testing expression dataset.
@@ -460,7 +351,8 @@ def test_score(tsv_file, base_path='/data', own_expr=False, mask='none', probe_s
     comp_vec = comp_mat[np.tril_indices(n=comp_mat.shape[0], k=-1)]
 
     if mask != "none":
-        v_mask = one_mask(expr, mask, bids_val('parby', tsv_file), base_path)
+        data = ge.Data(base_path, external_logger=null_handler)
+        v_mask = algorithms.one_mask(expr, mask, bids_val('parby', tsv_file), data, null_handler)
         expr_vec = expr_vec[v_mask]
         comp_vec = comp_vec[v_mask]
 
